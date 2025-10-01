d5f3f10b5ef99cb2195ea7739deb5b620727adfd
Sivanth-Raj <sivanthraj20@gmail.com>
2025-10-01 23:24:56 +0530

    fix: change params

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+""" from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -13,3 +13,19 @@ engine = create_engine(
     max_overflow=30,    # Allow some overflow
     pool_timeout=40     # Timeout in seconds before giving up getting connection
 )
+ """
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./taskapp.db"
+
+# Conditional engine creation due to SQLite limitations on pool args
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
